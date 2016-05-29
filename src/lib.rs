8b1db77e8ec67297f80acf03b245a4ebe1eefae3
Peter Atashian <retep998@gmail.com>
2016-05-29 00:31:40 -0400

    Nice improvements

--- src/lib.rs
+++ src/lib.rs
@@ -13,13 +13,14 @@ use hyper::error::Error as HyperError;
 use hyper::header::{ContentType, Cookie, SetCookie, UserAgent};
 use hyper::method::{Method};
 use hyper::status::{StatusCode};
-use rustc_serialize::json::{Array, Json, Object, ParserError};
+use rustc_serialize::json::{Array, DecoderError, Json, Object, ParserError, decode};
 use std::borrow::{Borrow};
 use std::cell::{RefCell};
 use std::collections::{HashMap};
-use std::io::{Read, Write};
-use std::io::Error as IoError;
+use std::fs::{File};
+use std::io::{Error as IoError, Read, Write};
 use std::marker::{PhantomData};
+use std::path::{Path};
 use std::thread::{sleep};
 use std::time::{Duration};
 use url::ParseError as UrlError;
@@ -28,6 +29,7 @@ use url::form_urlencoded::{Serializer};
 #[derive(Debug)]
 pub enum Error {
     Json(Json),
+    Decode(DecoderError),
     Url(UrlError),
     Hyper(HyperError),
     Io(IoError),
@@ -36,6 +38,11 @@ pub enum Error {
 impl From<Json> for Error {
     fn from(err: Json) -> Error {
         Error::Json(err)
+    }
+}
+impl From<DecoderError> for Error {
+    fn from(err: DecoderError) -> Error {
+        Error::Decode(err)
     }
 }
 impl From<UrlError> for Error {
@@ -119,6 +126,13 @@ impl Mediawiki {
         };
         try!(mw.do_login(None));
         Ok(mw)
+    }
+    pub fn login_file<P: AsRef<Path>>(path: P) -> Result<Mediawiki, Error> {
+        let mut config = try!(File::open(path));
+        let mut s = String::new();
+        try!(config.read_to_string(&mut s));
+        let config = try!(decode(&s));
+        Mediawiki::login(config)
     }
     fn do_login(&self, token: Option<&str>) -> Result<(), Error> {
         let mut args = vec![
@@ -241,6 +255,33 @@ impl Mediawiki {
         let json: Json = try!(Json::from_str(&body));
         Ok(json)
     }
+    pub fn add_tile(
+        &self, token: &Token<Csrf>, tsmod: &str, tsname: &str, tsx: u32, tsy: u32,
+    ) -> Result<Json, Error> {
+        let (x, y) = (tsx.to_string(), tsy.to_string());
+        let args = [
+            ("format", "json"), ("action", "addtile"), ("tstoken", &*token.0), ("tsmod", tsmod),
+            ("tsname", tsname), ("tsx", &*x), ("tsy", &*y),
+        ];
+        let mut resp = try!(self.post_request(&self.config.baseapi, &args));
+        let mut body = String::new();
+        try!(resp.read_to_string(&mut body));
+        let json: Json = try!(Json::from_str(&body));
+        Ok(json)
+    }
+    pub fn create_sheet(
+        &self, token: &Token<Csrf>, tsmod: &str, tssizes: &str
+    ) -> Result<Json, Error> {
+        let args = [
+            ("format", "json"), ("action", "createsheet"), ("tstoken", &*token.0),
+            ("tsmod", tsmod), ("tssizes", tssizes),
+        ];
+        let mut resp = try!(self.post_request(&self.config.baseapi, &args));
+        let mut body = String::new();
+        try!(resp.read_to_string(&mut body));
+        let json: Json = try!(Json::from_str(&body));
+        Ok(json)
+    }
 }
 pub struct Query<'a> {
     mw: &'a Mediawiki,
@@ -256,17 +297,7 @@ impl<'a> Query<'a> {
         try!(resp.read_to_string(&mut body));
         let json: Json = try!(Json::from_str(&body));
         let buf = try!(json.get("query").get(&self.name));
-        if let Ok(buf) = buf.array() {
-            self.buf.clone_from(buf);
-        } else if let Ok(buf) = buf.object() {
-            self.buf.extend(buf.iter().map(|(id, rest)| {
-                let mut json = rest.clone();
-                json.as_object_mut().unwrap().insert("id".into(), Json::String(id.clone()));
-                json
-            }));
-        } else {
-            unreachable!("{:?}", buf);
-        }
+        self.buf.clone_from(try!(buf.array()));
         self.buf.reverse();
         if let Ok(cont) = json.get("continue") {
             for (key, val) in try!(cont.object()) {
